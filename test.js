33fec63ebd7d1af8d4b2b1bde504dd44e411d5d7
itsretr0n <51875905+itsretr0n@users.noreply.github.com>
2019-08-13 00:51:42 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const dischook  =  require('../dischook/main');
 
 //Just as easy to send a normal message
-const webhookURL = 'https://discordapp.com/api/webhooks/608208394234363916/OlZPugIsllAd-nHlO_QWKQqLPZF88bqT7_XXF6Vr-lPDpvvHnWphbG1ubGgL-yID1Azz'
+const webhookURL = 'https://discordapp.com/api/webhooks/610700530607587339/R7AQWnmqzrBb3j8CY9nSl2pbKYKx2vlC94Lk-WzM8OFcinigDeegKA2WQEoQLgb_lmvc'
 const avatarURL = 'https://cdn.discordapp.com/avatars/516840368843522073/1040b30414894c8e427ccae7a96d3718.webp?size=128'
 new dischook(webhookURL,'Retr0n',avatarURL)
 	.setTitle('Im a title!')
